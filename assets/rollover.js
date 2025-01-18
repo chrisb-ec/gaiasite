fd3f30667a9d07f7040c67d92e47c93fff9deafe
coolpringles <ec2130494@edinburghcollege.ac.uk>
2025-01-18 20:56:57 +0000

    add site

---
+++ assets/rollover.js
@@ -0,0 +1,11 @@
+// The script for the rollover and whatnot
+
+function mouseRollover(myImage)
+{
+    myImage.src = "assets/images/weblogo-ns.png"
+}
+
+function mouseOut(myImage)
+{
+    myImage.src = "assets/images/weblogo.png"
+}
